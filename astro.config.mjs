7e88f6ec3ff2d08c9254cfe55e4bc40155d49464
Amadeusz Zaradny <45848839+AmadeuszZaradny@users.noreply.github.com>
2025-12-01 15:23:32 +0100

    Google search (#2)
    * added to google search
    
    * sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wyrolowani-torun.pl',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
 });
